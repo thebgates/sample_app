a928735ff9006aaefd7c1647f6a10b1983c9f774
Brian Gates <thebgates@gmail.com>
2013-06-06 11:37:34 -0700

    Make contacts page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,10 +23,10 @@ describe "Static pages" do
 			page.should have_selector('h1', :text => 'Help')
 		end
 
-			it "should have the title 'Help'" do
-				visit '/static_pages/help'
-				page.should have_selector('title',
-					:text => "Ruby on Rails Tutorial Sample App | Help")
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 
@@ -36,10 +36,25 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => 'About Us')
 		end
+
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
 				:text => "Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+
+	describe "Contact page" do
+
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', 
+				:text => "Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
